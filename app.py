ce5472ce5a1136789c84bb6cc7662fc1e97e7674
antoniovsilva <antonio.v.silva@grupoboticario.com.br>
2025-06-11 21:28:26 -0300

    feat: initial flask restful project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        return {"message":"user 1"}
+    
+class User(Resource):
+    def post(self):
+        return {"message": "teste"}
+    
+    def get(self, cpf: str):
+        return {"message": "CPF"}
+
+api.add_resource(Users, '/Users')
+api.add_resource(User, '/User/<string:cpf>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
