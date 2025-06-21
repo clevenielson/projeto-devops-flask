f0a800d6bed611aabde081d8379a2fa6d993b1dc
antoniovsilva <antonio.v.silva@grupoboticario.com.br>
2025-06-21 11:51:19 -0300

    feat: reqparse

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ APP = restapi
 test:
 	@flake8 . --exclude .venv
 
-compose:
+compose: test
 	@docker-compose build
 	@docker-compose up
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
@@ -12,10 +12,43 @@ app.config['MONGODB_SETTINGS'] = {
     'password': 'senha123'
 }
 
+_user_parse = reqparse.RequestParser()
+_user_parse.add_argument(
+    'first_name',
+    type=str,
+    required=True,
+    help="This field cannot be blank."
+)
+_user_parse.add_argument(
+    'last_name',
+    type=str,
+    required=True,
+    help="This field cannot be black."
+)
+_user_parse.add_argument(
+    'cpf',
+    type=str,
+    required=True,
+    help="This field cannot be black."
+)
+_user_parse.add_argument(
+    'email',
+    type=str,
+    required=True,
+    help="This field cannot be black."
+)
+_user_parse.add_argument(
+    'birth_date',
+    type=str,
+    required=True,
+    help="This field cannot be black."
+)
+
 api = Api(app)
 db = MongoEngine(app)
 
 
+# Modelo de usuario
 class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
     email = db.StringField(required=True)
@@ -31,14 +64,15 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"message": "teste"}
+        data = _user_parse.parse_args()
+        UserModel(**data).save()
 
     def get(self, cpf: str):
         return {"message": "CPF"}
 
 
-api.add_resource(Users, '/Users')
-api.add_resource(User, '/User/<string:cpf>')
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
